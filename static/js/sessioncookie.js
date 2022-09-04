d573c2dcf92c7ce1de8e953392a362175e1cf752
jonathanw <jonwhewaycode@outlook.com>
2022-09-04 17:01:51 +0100

    initial commit

---
+++ static/js/sessioncookie.js
@@ -0,0 +1,33 @@
+
+// when the onclick(clear_modal()) function is called from the news letter signup modal it adds warning=clicked to the session cookie + expiry
+// in this case its 100 days.
+/* number of seconds in a day. 24 * 60 * 60 = 86400 sec, 1 sec = 1000 milliseconds, so after calculating expression,result is in milliseconds
+   days = 100, (days * 24 * 60 * 60 * 1000 = 100 * 86400000 ms)
+*/
+
+// function clear_modal(){
+//   // let d = new Date();
+//   // let key = 'signup';
+//   // let value = true;  
+//   // d.setTime(d.getTime() + (100 * 24 * 60 * 60 * 1000));
+//   // var expires = "expires="+d.toUTCString();
+//   // document.cookie = key + "=" + value + ";" + expires + ";path=/";
+// }
+
+// /* When the landing page is first scrolled down, a session cookie is checked to see if there is an 
+// entry that displays the modal, if the modal has been displayed */
+
+// $(window).scroll(function(){
+  
+//   if ($(this).scrollTop() > 100) {
+//     let x = document.cookie;
+//     let b = x.split(';')
+//     .map(cookie => cookie.split('='))
+//     .reduce((accumulator, [key, value]) => 
+//       ({ ...accumulator, [key.trim()]: decodeURIComponent(value) }), 
+//       {});
+//     if (b.gdpr_consent){
+//       if (b.signup != true){
+//           $("#newslettersignup").modal('show');
+//       }else {}}
+// }});
---
+++ static/js/subscribe.js
@@ -0,0 +1,55 @@
+
+// This function takes the page_from and converts it to a global variable
+var globalpagename;
+function passedpage(page_from){
+    window.globalpagename = page_from;
+}
+
+
+/* This function looks to see if the css has changed in this case if 
+the css has gone from hidden to block then it is executed */
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutationRecord) {
+        document.getElementById('redirectToWebsiteButton').innerHTML = `<button class="btn btn-secondary buttonsclick button" onclick="backbut()" >Back</button>`;
+    });    
+});
+var target = document.getElementById('mce-error-response');
+observer.observe(target, { attributes : true, attributeFilter : ['style'] });
+
+
+var observer2 = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutationRecord) {
+        document.getElementById('autoRedirectMessage').innerHTML = `<p>Please Wait You Will be Automatically Redirected!</p>
+        <p>Incase you are not automatically redirected please <a href="#" onclick="backbut(globalpagename)">Click Here!</a></p>`;
+        document.getElementById('backbuttondiv').scrollIntoView({behavior: 'smooth'});
+        document.getElementById('subscribeButton').innerHTML = "";
+        autoRedirect();
+    });    
+});
+var target2 = document.getElementById('mce-success-response');
+observer2.observe(target2, { attributes : true, attributeFilter : ['style'] });
+
+// After 3 seconds the timeout triggers the backbut function and redirects to the landing page
+function autoRedirect(){
+    setTimeout(backbut, 3000);
+}
+
+/* This function looks to see what the globalpagename is and then goes to that url useing
+ Flask Jsglue */
+function backbut(){
+    if (globalpagename == 'about'){
+        window.location.replace(Flask.url_for('about'));
+    }
+    else if (globalpagename == 'index'){
+        window.location.replace(Flask.url_for('index'));
+    }
+    else if (globalpagename == 'contact'){
+        window.location.replace(Flask.url_for('contact'));
+    }
+    else if (globalpagename == 'subscribe'){
+        window.location.replace(Flask.url_for('subscribe'));
+    }
+    else if (globalpagename == 'None'){
+        window.location.replace(Flask.url_for('index'));
+    }
+}
