93d3f65888792830540c8297fa996841d8c58993
jonathanw <jonwhewaycode@outlook.com>
2022-09-10 15:48:35 +0100

    sort out subscribe page

--- static/js/subscribe.js
+++ static/js/subscribe.js
@@ -1,16 +1,11 @@
-
-// This function takes the page_from and converts it to a global variable
-var globalpagename;
-function passedpage(page_from){
-    window.globalpagename = page_from;
-}
+"use strict";
 
 
 /* This function looks to see if the css has changed in this case if 
 the css has gone from hidden to block then it is executed */
 var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutationRecord) {
-        document.getElementById('redirectToWebsiteButton').innerHTML = `<button class="btn btn-secondary buttonsclick button" onclick="backbut()" >Back</button>`;
+        document.getElementById('redirectToWebsiteButton').innerHTML = `<button class="btn btn-secondary buttonsclick button" href="subscribe.html" >Back</button>`;
     });    
 });
 var target = document.getElementById('mce-error-response');
@@ -20,7 +15,7 @@ observer.observe(target, { attributes : true, attributeFilter : ['style'] });
 var observer2 = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutationRecord) {
         document.getElementById('autoRedirectMessage').innerHTML = `<p>Please Wait You Will be Automatically Redirected!</p>
-        <p>Incase you are not automatically redirected please <a href="#" onclick="backbut(globalpagename)">Click Here!</a></p>`;
+        <p>Incase you are not automatically redirected please <a href="#" onclick="back()">Click Here!</a></p>`;
         document.getElementById('backbuttondiv').scrollIntoView({behavior: 'smooth'});
         document.getElementById('subscribeButton').innerHTML = "";
         autoRedirect();
@@ -31,26 +26,11 @@ observer2.observe(target2, { attributes : true, attributeFilter : ['style'] });
 
 // After 3 seconds the timeout triggers the backbut function and redirects to the landing page
 function autoRedirect(){
-    setTimeout(backbut, 3000);
+    setTimeout(back, 3000);
 }
 
 /* This function looks to see what the globalpagename is and then goes to that url useing
  Flask Jsglue */
-function backbut(){
-    
-    if (globalpagename == 'about'){
-        window.location.replace("about.html");
-    }
-    else if (globalpagename == 'index'){
-        window.location.replace("./index.html");
-    }
-    else if (globalpagename == 'contact'){
-        window.location.replace("contact.html");
-    }
-    else if (globalpagename == 'subscribe'){
-        window.location.replace("subscribe.html");
-    }
-    else if (globalpagename == 'None'){
-        window.location.replace("./index.html");
-    }
+function back(){
+    window.location.replace("contact.html");
 }
